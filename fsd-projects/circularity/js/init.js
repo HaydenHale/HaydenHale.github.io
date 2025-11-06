14c7147da49528e1cad776ac32d58396b0af985e
HaydenHale <hale977@wearescpps.org>
2025-11-06 17:52:08 +0000

    finished circularity project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -84,7 +84,7 @@ var init = function (window) {
       if (circle.y < 0) {
         circle.y = canvas.height;
       }
-      if (circle.y > canvas.hight) {
+      if (circle.y > canvas.height) {
         circle.y = 0;
       }
       if (circle.x < 0) {
